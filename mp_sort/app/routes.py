e659dbf792448a1068da4b5bd3f7277890915395
Natalie Agus <nn88blue@gmail.com>
2024-09-27 12:24:09 +0800

    feat: create special custom route for js files and manually set the response headers

--- mp_sort/app/routes.py
+++ mp_sort/app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, send_from_directory, make_response
 from app import application
 
 @application.route('/')
@@ -13,3 +13,10 @@ def exercise1():
 @application.route('/ex2')
 def exercise2():
     return render_template('ex2.html', title='Mini Project 1 Exercise 2')
+
+@application.route('/js/<path:filename>') # dynamic route parameter syntax
+def serve_js(filename):
+    # Manually set the Content-Type for JavaScript files
+    response = make_response(send_from_directory('static/__target__', filename))
+    response.headers['Content-Type'] = 'application/javascript'
+    return response
